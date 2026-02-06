aa47a9b20cb2fb73247564fe837b66d49927ffbf
Fagbuyi Ayomide <fagbuyiayomide5@gmail.com>
2026-02-06 16:22:34 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,25 +1,16 @@
 const express = require('express')
-
 require('dotenv').config()
 const cookieParser = require('cookie-parser')
 const createMsg = require('./controllers/createMsg')
 
-
-
-
-
-
 const app = express()
-const port = process.env.PORT || 9000
 
 app.use(express.json())
-app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({ extended: true }))
 app.use(cookieParser())
 
-//router
+// routes
 app.use('/api', createMsg)
 
-
-app.listen(port, () => {
-    console.log(`Server is listening on ${port} successfully `)
-})
+// IMPORTANT: export app for Vercel
+module.exports = app
