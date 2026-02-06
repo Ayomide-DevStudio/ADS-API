96ac7bb48f00e98ccec98d7442bb6cd69e1d0646
Fagbuyi Ayomide <fagbuyiayomide5@gmail.com>
2026-02-06 16:37:47 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const express = require('express')
-
 const cookieParser = require('cookie-parser')
 const createMsg = require('./controllers/createMsg')
 
@@ -9,8 +8,12 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cookieParser())
 
-// routes
+// Health check route (fixes Cannot GET /)
+app.get('/', (req, res) => {
+  res.send('ADS API is running 🚀')
+})
+
+// API routes (DO NOT COMMENT THIS OUT)
 app.use('/api', createMsg)
 
-// IMPORTANT: export app for Vercel
 module.exports = app
