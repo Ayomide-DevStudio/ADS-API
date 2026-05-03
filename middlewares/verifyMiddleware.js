97cd73ca2f2805c2f0ae3fb4b18ae0d664f01622
Fagbuyi Ayomide <fagbuyiayomide5@gmail.com>
2026-05-03 20:29:36 +0100

    Update verifyMiddleware.js

--- middlewares/verifyMiddleware.js
+++ middlewares/verifyMiddleware.js
@@ -1,5 +1,5 @@
 const verifyApiKey = (req, res, next) => {
-    const apiKey =  req.header['ADS-api-key']
+    const apiKey =  req.headers['ADS-api-key']
         if (!apiKey) return res.status(401).json({message: "Invalid Key"})
             next()
 }
