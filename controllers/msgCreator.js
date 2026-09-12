c30946efee2006cf3ab3a5f2195f913d57a77ba8
Fagbuyi Ayomide <fagbuyiayomide5@gmail.com>
2026-09-12 20:45:36 +0100

    Update msgCreator.js

--- controllers/msgCreator.js
+++ controllers/msgCreator.js
@@ -1,26 +1,68 @@
+const { adsMailSender } = require('../lib/adsmailSender');
 
-const {adsMailSender} = require('../lib/adsmailSender')
+const msgCreator = async (req, res) => {
 
-const msgCreator= async (req, res) => {
-        const {  to, subject, html} = req.body
-       
-        if( !to|| !subject || !html) return res.status(400).json({message: "Missing required fields"})
-            try {           
-                // to send mail 
-                const mailObj = {
-                    mailTo: to,
-                    subject: subject,
-                    html
-                }
-                const sent = await adsMailSender(mailObj)
-                if (!sent) return res.status(400).json({message: "Request Failed!"})
-                res.status(200).json({success: true, message: 'Email sent successfully'})
-            } catch (error) {
-                res.status(500).json({
-                        success: false,
-                    message: error.message
-                })
+    console.log('METHOD:', req.method);
+    console.log('BODY:', req.body);
+
+    const { to, subject, html } = req.body || {};
+
+    console.log('FIELDS:', {
+        to,
+        subject,
+        hasHtml: !!html,
+        htmlType: typeof html
+    });
+
+    if (!to || !subject || !html) {
+        return res.status(400).json({
+            success: false,
+            message: 'Missing required fields',
+            received: {
+                hasTo: !!to,
+                hasSubject: !!subject,
+                hasHtml: !!html
             }
-}
+        });
+    }
 
-module.exports = msgCreator
+    try {
+
+        const mailObj = {
+            mailTo: to,
+            subject,
+            html
+        };
+
+        console.log('MAIL OBJECT FOR SMTP:', {
+            mailTo: mailObj.mailTo,
+            subject: mailObj.subject,
+            hasHtml: !!mailObj.html
+        });
+
+        const sent = await adsMailSender(mailObj);
+
+        if (!sent) {
+            return res.status(400).json({
+                success: false,
+                message: 'Request Failed!'
+            });
+        }
+
+        return res.status(200).json({
+            success: true,
+            message: 'Email sent successfully'
+        });
+
+    } catch (error) {
+
+        console.error('msgCreator ERROR:', error);
+
+        return res.status(500).json({
+            success: false,
+            message: error.message
+        });
+    }
+};
+
+module.exports = msgCreator;
