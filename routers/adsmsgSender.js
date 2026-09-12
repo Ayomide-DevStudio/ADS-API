aa8c8dd8f3cbae7c72e9691c51d9171709383fc9
Fagbuyi Ayomide <fagbuyiayomide5@gmail.com>
2026-09-12 19:16:49 +0100

    Update adsmsgSender.js

--- routers/adsmsgSender.js
+++ routers/adsmsgSender.js
@@ -1,1 +1,11 @@
 
+const express = require('express')
+
+const createMsg =  require('../controllers/createMsg')
+const verifyApiKey = require('../middlewares/verifyMiddleware')
+
+
+const otpRoute =  express.Router()
+
+otpRoute 
+    .post('/send', verifyApiKey, createMsg)
