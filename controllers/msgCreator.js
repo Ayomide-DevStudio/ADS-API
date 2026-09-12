fd062e118b4f0b8368c6bdb9caef5512d6c5cfa0
Fagbuyi Ayomide <fagbuyiayomide5@gmail.com>
2026-09-12 19:20:21 +0100

    Update msgCreator.js

--- controllers/msgCreator.js
+++ controllers/msgCreator.js
@@ -1,1 +1,27 @@
 
+const {mailSender} = require('../lib/sendMail')
+
+const createMsg = async (req, res) => {
+        const { senderEmail, recipientEmail, subject, htmlContent} = req.body
+       
+        if(!senderEmail || !recipientEmail || !subject || !htmlContent) return res.status(400).json({message: "Missing required fields"})
+            try {           
+                // to send mail 
+                const mailObj = {
+                    mailFrom: senderEmail,
+                    mailTo: recipientEmail,
+                    subject: subject,
+                    body: htmlContent
+                }
+                const sent = await mailSender(mailObj)
+                if (!sent) return res.status(400).json({message: "Request Failed!"})
+                res.status(200).json({success: true, message: 'Email sent successfully'})
+            } catch (error) {
+                res.status(500).json({
+                        success: false,
+                    message: error.message
+                })
+            }
+}
+
+module.exports = createMsg
